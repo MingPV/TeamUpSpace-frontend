8d349f18b3b653684088824eb4f0fbaff09b6e47
Aunyapat Nitijarasrat <aunyapat2003@gmail.com>
2025-08-27 10:03:54 +0700

    remove unused in MobileNavbar

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -10,21 +10,9 @@ import Image from "next/image";
 
 export default function MobileNav() {
   const [isOpen, setIsOpen] = useState(false);
-  const [isDark, setIsDark] = useState(false);
 
   const toggleSidebar = () => setIsOpen((prev) => !prev);
-  const toggleTheme = () => {
-    const root = document.documentElement;
-    if (root.classList.contains("dark")) {
-      root.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-      setIsDark(false);
-    } else {
-      root.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      setIsDark(true);
-    }
-  };
+
   return (
     <div>
       {/* Toggle button */}
