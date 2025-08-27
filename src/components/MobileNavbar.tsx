2e5588ff9ed23bbbc8fb3accb064f2d144cb4f81
Aunyapat Nitijarasrat <aunyapat2003@gmail.com>
2025-08-27 10:02:07 +0700

    add sidebar and modify toggle theme

---
+++ src/components/MobileNavbar.tsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross1 } from "react-icons/rx";
+import { IoHomeSharp } from "react-icons/io5";
+import { FaUserFriends } from "react-icons/fa";
+import { MdEmojiEvents } from "react-icons/md";
+import { IoNotifications } from "react-icons/io5";
+import { AiOutlineMessage } from "react-icons/ai";
+import Image from "next/image";
+
+export default function MobileNav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleSidebar = () => setIsOpen((prev) => !prev);
+  const toggleTheme = () => {
+    const root = document.documentElement;
+    if (root.classList.contains("dark")) {
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDark(false);
+    } else {
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDark(true);
+    }
+  };
+  return (
+    <div>
+      {/* Toggle button */}
+      <div
+        onClick={toggleSidebar}
+        className="p-2 cursor-pointer hover:bg-black/10 dark:hover:bg-white/10 rounded-md mb-1 ml-4 flex items-center justify-center text-2xl"
+      >
+        <span
+          className={`transition-transform duration-300 ${
+            isOpen ? "rotate-90" : "rotate-0"
+          }`}
+        >
+          {isOpen ? <RxCross1 /> : <GiHamburgerMenu />}
+        </span>
+      </div>
+
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 right-0 h-screen w-1/2 bg-white shadow-lg transform transition-transform duration-300 lg:hidden z-50 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="p-6 flex flex-col gap-4" onClick={toggleSidebar}>
+          <div className="flex w-full items-end justify-end">
+            {isOpen ? <RxCross1 /> : <GiHamburgerMenu />}
+          </div>
+
+          <div className="flex flex-row py-2 border-b-2 cursor-pointer items-center justify-start ">
+            <IoHomeSharp className="text-xl text-base-500" />
+            <div className="text-sm px-3  text-base-400">Home</div>
+          </div>
+          <div className="flex flex-row py-2 border-b-2 border-b-transparent hover:border-b-base-500 cursor-pointer items-center justify-start  text-base-400/70 hover:text-base-500">
+            <FaUserFriends className="text-xl" />
+            <div className="text-sm px-3">Friends</div>
+          </div>
+          <div className="flex flex-row py-2 border-b-2 border-b-transparent hover:border-b-base-500 cursor-pointer items-center justify-start  text-base-400/70 hover:text-base-500">
+            <MdEmojiEvents className="text-xl " />
+            <div className="text-sm px-3  ">Events</div>
+          </div>
+          <div className="flex flex-row py-2 border-b-2 border-b-transparent hover:border-b-base-500 cursor-pointer items-center justify-start  text-base-400/70 hover:text-base-500">
+            <AiOutlineMessage className="text-xl " />
+            <div className="text-sm px-3  ">Messages</div>
+          </div>
+          <div className="flex flex-row py-2 border-b-2 border-b-transparent hover:border-b-base-500 cursor-pointer items-center justify-start  text-base-400/70 hover:text-base-500">
+            <IoNotifications className="text-xl " />
+            <div className="text-sm px-3  ">Notification</div>
+          </div>
+          <div className="flex flex-row  border-b-2 border-b-transparent hover:border-b-base-500 cursor-pointer items-center justify-start  text-base-400/70 hover:text-base-500">
+            <div className="flex flex-row px-0 pt-1 hover:bg-black/10 cursor-pointer items-center rounded-md">
+              <Image
+                src={"/golang.webp"}
+                width={100}
+                height={100}
+                alt="profile-pic"
+                style={{ objectFit: "cover" }}
+                className="rounded-full h-7 w-7"
+              />
+              <div className="flex items-center">
+                <div className="text-sm px-3 ">Profile</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Overlay */}
+      {isOpen && (
+        <div
+          onClick={toggleSidebar}
+          className="fixed inset-0 bg-black/40 lg:hidden z-40"
+        />
+      )}
+    </div>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { BsSun } from "react-icons/bs";
-import { RxMoon } from "react-icons/rx";
+import { useState } from "react";
 import { IoHomeSharp } from "react-icons/io5";
 import { FaUserFriends } from "react-icons/fa";
 import { MdEmojiEvents } from "react-icons/md";
@@ -11,51 +9,23 @@ import { AiOutlineMessage } from "react-icons/ai";
 import { FaCaretDown } from "react-icons/fa";
 import { IoMdSearch } from "react-icons/io";
 import Image from "next/image";
-
+import MobileNav from "./MobileNavbar";
+import Switch from "./switch";
 export default function Navbar() {
   const [isDark, setIsDark] = useState(false);
-
-  // Sync theme on mount
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "dark") {
-      document.documentElement.classList.add("dark");
-      setIsDark(true);
-    } else if (savedTheme === "light") {
-      document.documentElement.classList.remove("dark");
-      setIsDark(false);
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      document.documentElement.classList.add("dark");
-      setIsDark(true);
-    }
-  }, []);
-
-  // Toggle dark mode
-  const toggleTheme = () => {
-    const root = document.documentElement;
-    if (root.classList.contains("dark")) {
-      root.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-      setIsDark(false);
-    } else {
-      root.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      setIsDark(true);
-    }
-  };
 
   return (
     <nav
       className={`
         fixed top-0 left-0 w-full z-40 bg-white dark:bg-base-500 shadow-md
-        dark:text-base-100 text-base-500 flex justify-center items-center pt-1 gap-20
+        dark:text-base-100 text-base-500 flex justify-between sm:justify-center items-center pt-1 gap-20
       `}
     >
-      <div className="flex gap-2 items-center mb-1">
+      <div className="flex gap-2 items-center mb-1 px-2 ">
         <div className="p-2 font-bold text-base-400 rounded-md cursor-pointer">
           TeamUp-Space
         </div>
-        <div className="flex gap-2 px-2 w-72 border border-base-300 rounded-full py-1.5 items-center">
+        <div className="hidden sm:flex gap-2 px-2 w-72 border border-base-300 rounded-full py-1.5 items-center">
           <IoMdSearch />
           <input
             type="text"
@@ -64,7 +34,7 @@ export default function Navbar() {
           />
         </div>
       </div>
-      <div className="hidden md:flex justify-center items-center">
+      <div className="flex flex-row justify-center items-center ">
         <div className="hidden lg:flex flex-col py-2 border-b-2 cursor-pointer items-center px-6">
           <IoHomeSharp className="text-xl text-base-500" />
           <div className="text-sm  text-base-400">Home</div>
@@ -85,8 +55,8 @@ export default function Navbar() {
           <IoNotifications className="text-xl " />
           <div className="text-sm  ">Notification</div>
         </div>
-        <div className="flex flex-col border-b-2 border-b-white items-center px-8 border-r border-r-base-200 ">
-          <div className="flex flex-col px-4 pt-1 hover:bg-black/10 cursor-pointer items-center rounded-md">
+        <div className="hidden lg:flex flex-col border-b-2 border-b-white items-center px-8 border-r border-r-base-200 ">
+          <div className="flex flex-col px-0 md:px-4 pt-1 hover:bg-black/10 cursor-pointer items-center rounded-md">
             <Image
               src={"/golang.webp"}
               width={100}
@@ -101,17 +71,14 @@ export default function Navbar() {
             </div>
           </div>
         </div>
-        <button
-          onClick={toggleTheme}
-          aria-label="Toggle Dark Mode"
-          className="p-2 cursor-pointer hover:bg-black/10 dark:hover:bg-white/10 rounded-md mb-1 ml-8"
-        >
-          {isDark ? (
-            <BsSun className="text-amber-400 text-xl" />
-          ) : (
-            <RxMoon className="text-indigo-500 text-xl" />
-          )}
-        </button>
+
+        <div className="flex flex-col py-2  cursor-pointer items-center px-2 text-base-400/70 hover:text-base-500 scale-60">
+          <Switch />
+        </div>
+
+        <div className="lg:hidden p-0 cursor-pointer hover:bg-black/10 dark:hover:bg-white/10 rounded-md mb-1 sm:ml-8 text-2xl flex items-center justify-center">
+          <MobileNav />
+        </div>
       </div>
     </nav>
   );
