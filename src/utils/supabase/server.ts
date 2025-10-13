3dba8c91738531903d489b7e0502e32f66a30b50
MingPV <jeung.pavee@gmail.com>
2025-10-13 09:36:28 +0700

    fix header too large

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,28 +1,45 @@
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
+// import { createServerClient } from "@supabase/ssr";
+// import { cookies } from "next/headers";
+
+// export const createClient = async () => {
+//   const cookieStore = await cookies();
+
+//   return createServerClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//     {
+//       cookies: {
+//         getAll() {
+//           return cookieStore.getAll();
+//         },
+//         setAll(cookiesToSet) {
+//           try {
+//             cookiesToSet.forEach(({ name, value, options }) => {
+//               cookieStore.set(name, value, options);
+//             });
+//           } catch (error) {
+//             // The `set` method was called from a Server Component.
+//             // This can be ignored if you have middleware refreshing
+//             // user sessions.
+//           }
+//         },
+//       },
+//     }
+//   );
+// };
+
+// lib/supabaseClient.ts
+import { createClient as CreateClient } from "@supabase/supabase-js";
 
 export const createClient = async () => {
-  const cookieStore = await cookies();
-
-  return createServerClient(
+  return CreateClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) => {
-              cookieStore.set(name, value, options);
-            });
-          } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
+      auth: {
+        persistSession: false,
+        autoRefreshToken: false,
+        detectSessionInUrl: true,
       },
     }
   );
